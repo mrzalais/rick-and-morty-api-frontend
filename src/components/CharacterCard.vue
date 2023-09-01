0d0cafc3c60341bda86e81ba0952d30eea868c5f
mrzalais <lejnieksedijs@gmail.com>
2023-09-01 14:26:10 +0300

    Extract character details to separate component

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="character-card">
+    <div class="character-image">
+      <img :src="character.image" :alt="character.name" />
+    </div>
+    <div class="character-details">
+      <h3>{{ character.name }}</h3>
+      <p>Species: {{ character.species }}</p>
+      <p>Status: {{ character.status }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    character: Object,
+  },
+};
+</script>
+
+<style scoped>
+.character-card {
+  display: inline-block;
+  width: 25%;
+  box-sizing: border-box;
+  margin: 10px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  background-color: #f9f9f9;
+}
+
+.character-image img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+  margin: 0 auto;
+}
+
+.character-details {
+  text-align: center;
+}
+</style>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,20 +1,12 @@
 <template>
   <div>
-    <div class="character-card" v-for="character in characters" :key="character.id">
-      <div class="character-image">
-        <img :src="character.image" :alt="character.name"/>
-      </div>
-      <div class="character-details">
-        <h3>{{ character.name }}</h3>
-        <p>Species: {{ character.species }}</p>
-        <p>Status: {{ character.status }}</p>
-      </div>
-    </div>
+    <CharacterCard v-for="character in characters" :key="character.id" :character="character" />
   </div>
 </template>
 
 <script setup>
 import useCharacters from "@/api/characters"
+import CharacterCard from "@/components/CharacterCard.vue";
 import { onMounted } from "vue"
 
 const { characters, getCharacters } = useCharacters()
@@ -23,26 +15,3 @@ onMounted(() => {
   getCharacters()
 })
 </script>
-
-<style scoped>
-.character-card {
-  display: inline-block;
-  width: 25%;
-  box-sizing: border-box;
-  margin: 10px;
-  border: 1px solid #ccc;
-  padding: 10px;
-  background-color: #f9f9f9;
-}
-
-.character-image img {
-  max-width: 100%;
-  height: auto;
-  display: block;
-  margin: 0 auto;
-}
-
-.character-details {
-  text-align: center;
-}
-</style>
