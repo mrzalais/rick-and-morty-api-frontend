f0567d273cf01081692b022ff2fb6cd7c703d139
mrzalais <lejnieksedijs@gmail.com>
2023-09-07 21:20:01 +0300

    Add additional info on character page

--- src/components/CharacterPage.vue
+++ src/components/CharacterPage.vue
@@ -5,6 +5,12 @@
         <router-link to="/" class="nav-link">Back to character List</router-link>
       </nav>
       <CharacterCard :character="character" />
+      <div>
+        <h2>Extra Information</h2>
+        <p>Gender: {{ character.gender }}</p>
+        <p v-if="character.location">Location: {{ character.location.name }}</p>
+        <p v-if="character.origin">Origin: {{ character.origin.name }}</p>
+      </div>
     </template>
     <template v-else>
       <div>Loading...</div>
@@ -13,7 +19,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import useCharacters from '@/api/characters';
 import CharacterCard from "@/components/CharacterCard.vue";
@@ -21,10 +27,7 @@ import CharacterCard from "@/components/CharacterCard.vue";
 const { character, getCharacter, isLoading } = useCharacters()
 const route = useRoute()
 
-const characterLoaded = ref(false);
-
 onMounted(async () => {
   await getCharacter(route.params.id);
-  characterLoaded.value = true;
 })
 </script>
