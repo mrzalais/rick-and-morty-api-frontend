22d535b72d4ae581d08fe64721cea57356c2ebf4
mrzalais <lejnieksedijs@gmail.com>
2023-09-07 21:00:45 +0300

    Create router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import CharacterPage from '@/components/CharacterPage.vue'
+import CharacterList from '@/components/CharacterList.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', name: 'characterList', component: CharacterList },
+    { path: '/character/:id', name: 'character', component: CharacterPage },
+  ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
